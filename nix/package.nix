24c2e1b85e011a85e1c15c0413ff53cbdadd00f0
Ghastrum <dennis.malmin@tuta.com>
2026-04-18 17:23:11 +0200

    Updated to latest commit

--- nix/package.nix
+++ nix/package.nix
@@ -2,19 +2,19 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "fetch";
-  version = "1.0.0-unstable-18-04-1";
+  version = "1.0.0-unstable-18-04-2";
 
 
 # Uncomment to build locally
-src = ../.;
+# src = ../.;
 
 # Uncomment and change rev and hash when upstream fix for kitty has been pushed
-#src = fetchFromGitHub {
-#  owner = "areofyl";
-#  repo = "fetch";
-#  rev = "a056f655a57611cba4f1076cfa40dbf360da164f";
-#  hash = "sha256-508QYmyPSKZisABowgyaRV93ZrkTiS3ziKBCcs3WOpQ=";
-#};
+src = fetchFromGitHub {
+  owner = "areofyl";
+  repo = "fetch";
+  rev = "67ba7abd90d548e89a0a60bf7a850ee41f5c6f79";
+  hash = "sha256-20lZPymAgXmTAxxBs75E9es1ChHZ4RSS73Qb/SzYeiI=";
+};
 
 
   # Install in nix/store
