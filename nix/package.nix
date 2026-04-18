490f1266528616f8d72d1a479b9498509af36d56
Ghastrum <dennis.malmin@tuta.com>
2026-04-18 17:25:31 +0200

    Fix indents

--- nix/package.nix
+++ nix/package.nix
@@ -5,16 +5,15 @@ stdenv.mkDerivation (finalAttrs: {
   version = "1.0.0-unstable-18-04-2";
 
 
-# Uncomment to build locally
-# src = ../.;
+  # Uncomment to build locally
+  # src = ../.;
 
-# Uncomment and change rev and hash when upstream fix for kitty has been pushed
-src = fetchFromGitHub {
-  owner = "areofyl";
-  repo = "fetch";
-  rev = "67ba7abd90d548e89a0a60bf7a850ee41f5c6f79";
-  hash = "sha256-20lZPymAgXmTAxxBs75E9es1ChHZ4RSS73Qb/SzYeiI=";
-};
+  src = fetchFromGitHub {
+    owner = "areofyl";
+    repo = "fetch";
+    rev = "67ba7abd90d548e89a0a60bf7a850ee41f5c6f79";
+    hash = "sha256-20lZPymAgXmTAxxBs75E9es1ChHZ4RSS73Qb/SzYeiI=";
+  };
 
 
   # Install in nix/store
